85a44625511c3c07c0053e6e75ad376ede6e5de8
alexeykis1999 <63456842+alexeykis1999@users.noreply.github.com>
2021-08-23 22:07:14 +0300

    Add files via upload

---
+++ hw1/project_1/response_builder.rb
@@ -0,0 +1,21 @@
+# localhost:5678/other.html
+
+require_relative 'response'
+
+class ResponseBuilder
+  def build_response(request:)
+    if request[:path] == '/'
+      respond_with('views' + '/index.html')
+    else
+      respond_with('views' + request[:path])
+    end
+  end
+
+  def respond_with(path_to_file)
+    Response.new(
+      code: 200,
+      data: File.binread(path_to_file)
+    )
+    # TODO: add ability to return Response.new(code: 404) if path to file was not found
+  end
+end
